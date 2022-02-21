6e7d688c14369f7a572f68e88f9cbeecd1fed0e1
Joel Tumambo <joeltumambs@gmail.com>
2022-02-21 12:13:54 +0800

    Display values in TextField story

--- stories/components/TextField.stories.tsx
+++ stories/components/TextField.stories.tsx
@@ -23,61 +23,78 @@ export const AllTextFields = () => {
   return (
     <div
       style={{
-        display: "grid",
-        gap: "24px",
+        display: "flex",
       }}
     >
-      <TextField
-        id="input1"
-        value={values.input1}
-        onChange={(e) => setValues({ ...values, input1: e.target.value })}
-        label="Input"
-        placeholder="Placeholder"
-      />
-      <TextField
-        id="input2"
-        value={values.input2}
-        onChange={(e) => setValues({ ...values, input2: e.target.value })}
-        label="Input"
-        placeholder="Placeholder"
-        helper="Helper text"
-      />
-      <TextField
-        id="input3"
-        value={values.input3}
-        onChange={(e) => setValues({ ...values, input3: e.target.value })}
-        label="Input"
-        placeholder="Placeholder"
-        helper="Error text"
-        error
-      />
-      <TextField
-        id="textarea1"
-        value={values.textarea1}
-        onChange={(e) => setValues({ ...values, textarea1: e.target.value })}
-        label="Textarea"
-        placeholder="Placeholder text"
-        multiline
-      />
-      <TextField
-        id="textarea2"
-        value={values.textarea2}
-        onChange={(e) => setValues({ ...values, textarea2: e.target.value })}
-        label="Textarea"
-        placeholder="Placeholder text"
-        multiline
-        helper="Helper text"
-      />
-      <TextField
-        id="textarea3"
-        value={values.textarea3}
-        onChange={(e) => setValues({ ...values, textarea3: e.target.value })}
-        label="Textarea"
-        placeholder="Placeholder text"
-        multiline
-        helper="Error text"
-        error
-      />
+      <div
+        style={{
+          display: "grid",
+          gap: "24px",
+          flex: 1,
+        }}
+      >
+        <TextField
+          id="input1"
+          value={values.input1}
+          onChange={(e) => setValues({ ...values, input1: e.target.value })}
+          label="Input 1"
+          placeholder="Placeholder"
+        />
+        <TextField
+          id="input2"
+          value={values.input2}
+          onChange={(e) => setValues({ ...values, input2: e.target.value })}
+          label="Input 2"
+          placeholder="Placeholder"
+          helper="Helper text"
+        />
+        <TextField
+          id="input3"
+          value={values.input3}
+          onChange={(e) => setValues({ ...values, input3: e.target.value })}
+          label="Input 3"
+          placeholder="Placeholder"
+          helper="Error text"
+          error
+        />
+        <TextField
+          id="textarea1"
+          value={values.textarea1}
+          onChange={(e) => setValues({ ...values, textarea1: e.target.value })}
+          label="Textarea 1"
+          placeholder="Placeholder text"
+          multiline
+        />
+        <TextField
+          id="textarea2"
+          value={values.textarea2}
+          onChange={(e) => setValues({ ...values, textarea2: e.target.value })}
+          label="Textarea 2"
+          placeholder="Placeholder text"
+          multiline
+          helper="Helper text"
+        />
+        <TextField
+          id="textarea3"
+          value={values.textarea3}
+          onChange={(e) => setValues({ ...values, textarea3: e.target.value })}
+          label="Textarea 3"
+          placeholder="Placeholder text"
+          multiline
+          helper="Error text"
+          error
+        />
+      </div>
+      <code
+        style={{
+          flex: 1,
+          marginLeft: "16px",
+          display: "block",
+          whiteSpace: "pre-wrap",
+        }}
+      >
+        {JSON.stringify({ values }, null, 2)}
+      </code>
     </div>
   );
 };
