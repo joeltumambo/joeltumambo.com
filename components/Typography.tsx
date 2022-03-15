5bcf88e2e5b9d713022efac9c3de6ffc1878e4c0
Joel Tumambo <joeltumambs@gmail.com>
2022-03-15 12:26:01 +0800

    Add whitespace to Typoghraphy

--- components/Typography.tsx
+++ components/Typography.tsx
@@ -17,17 +17,27 @@ export type TypographyWeightType = 300 | 400 | 500 | 700 | 900;
 interface TypographyProps {
   component?: TypographyComponentType;
   size?: number;
+  lineHeight?: number;
   weight?: TypographyWeightType;
   gutter?: number;
   color?: "inherit" | "primary" | "neutral";
+  whiteSpace?:
+    | "normal"
+    | "pre"
+    | "nowrap"
+    | "pre-wrap"
+    | "pre-line"
+    | "break-spaces";
 }
 
 const Typography: React.FC<TypographyProps> = ({
   component = "span",
   size = 0,
   weight = 400,
+  lineHeight = 1,
   gutter = 1,
   color = "inherit",
+  whiteSpace = "normal",
   children,
 }) => {
   return React.createElement(
@@ -38,8 +48,9 @@ const Typography: React.FC<TypographyProps> = ({
         fontSize: `${1 * (1 + (1 / 8) * size)}rem`,
         marginBottom: `${gutter}em`,
         fontWeight: weight,
-        lineHeight: "1.25em",
-        color: color
+        lineHeight: `${1 * (1 + (1 / 4) * lineHeight)}em`,
+        color: color,
+        whiteSpace: whiteSpace,
       },
     },
     children
