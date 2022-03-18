370e6b4bbccde08ff8875ccfd8d2d26b5a88ee83
Joel Tumambo <joeltumambs@gmail.com>
2022-03-18 11:42:42 +0800

    Add disabled and type to TextField

--- atoms/TextField.tsx
+++ atoms/TextField.tsx
@@ -1,26 +1,31 @@
 import React, { ChangeEventHandler } from "react";
+import classnames from "classnames";
 import styles from "../styles/TextField.module.css";
 
 interface TextFieldProps {
   id: string;
   label: string;
   value: string;
+  type?: "text" | "email";
   onChange: ChangeEventHandler<HTMLInputElement | HTMLTextAreaElement>;
   placeholder?: string;
   multiline?: boolean;
   error?: boolean;
   helper?: string;
+  disabled?: boolean;
 }
 
 const TextField: React.FC<TextFieldProps> = ({
   id,
   label,
   value,
+  type = "text",
   onChange,
   placeholder,
   multiline,
   helper,
   error,
+  disabled,
 }) => {
   const input = (
     <input
@@ -28,6 +33,7 @@ const TextField: React.FC<TextFieldProps> = ({
       placeholder={placeholder}
       value={value}
       onChange={onChange}
+      type={type}
     />
   );
   const textArea = (
@@ -41,7 +47,7 @@ const TextField: React.FC<TextFieldProps> = ({
   );
 
   return (
-    <div className={styles.container}>
+    <div className={classnames(styles.container, disabled && styles.disabled)}>
       <label htmlFor={id}>{label}</label>
       {multiline ? textArea : input}
       {helper && <span className={error ? styles.error : ""}>{helper}</span>}
