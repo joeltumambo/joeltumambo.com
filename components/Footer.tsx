85c3423dca85ad9b830d77aaaa131ce761c85d49
Joel Tumambo <joeltumambs@gmail.com>
2022-03-30 19:07:09 +0800

    Fix Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,13 +1,20 @@
 import Typography from "./Typography";
 import styles from "../styles/Home.module.css";
+import Container from "./Container";
 
 const Footer = () => (
-  <footer className={styles.footer}>
-    <div>
+  <footer
+    style={{
+      background: "#212121",
+      color: "#9e9e9e",
+      padding: "16px 0",
+    }}
+  >
+    <Container height="48px">
       <Typography gutter={0} color="inherit" size={{ xs: -2, sm: -1 }}>
         © {new Date().getFullYear()} Joel Tumambo
       </Typography>
-    </div>
+    </Container>
   </footer>
 );
 
