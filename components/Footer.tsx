13b2af99bafebd326c50417fd8a39e8dac75fdad
Joel Tumambo <joeltumambs@gmail.com>
2022-03-31 01:14:52 +0800

    Fix Footer height

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,16 +1,16 @@
 import Typography from "./Typography";
 import styles from "../styles/Home.module.css";
 import Container from "./Container";
+import Grid from "./Grid";
 
 const Footer = () => (
   <footer
     style={{
       background: "#212121",
       color: "#9e9e9e",
-      padding: "16px 0",
     }}
   >
-    <Container minHeight="48px">
+    <Container minHeight="auto">
       <Typography gutter={0} color="inherit" size={{ xs: -2, sm: -1 }}>
         © {new Date().getFullYear()} Joel Tumambo
       </Typography>
