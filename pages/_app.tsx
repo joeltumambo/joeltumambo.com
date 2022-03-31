560c36d4d088ecf2d463215c54258625983ead99
Joel Tumambo <joeltumambs@gmail.com>
2022-04-01 00:44:50 +0800

    Add viewport meta tag and use device height

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,21 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { useEffect } from "react";
+
+const makeEven = (n: number) => n % 2 === 0 ? n : n + 1;
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    const metaViewport = document.querySelector("meta[name=viewport]");
+    if (metaViewport) {
+      metaViewport.setAttribute(
+        "content",
+        `height=${makeEven(innerHeight)}, width=device-width, initial-scale=1.0`
+      );
+    }
+  }, []);
+
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
