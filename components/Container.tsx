60b674840bc19f3ca71a69d34de54adc7d8d7740
Joel Tumambo <joeltumambs@gmail.com>
2022-04-01 18:57:20 +0800

    Lazy load sections

--- components/Container.tsx
+++ components/Container.tsx
@@ -3,30 +3,32 @@ import styles from "../styles/Container.module.css";
 
 interface ContainerProps {
   id?: string;
+  containerRef?: React.Ref<HTMLElement>;
   component?: "section" | "div";
   background?: string;
-  minHeight?: string
+  minHeight?: string;
 }
 
 const Container: React.FC<ContainerProps> = ({
   id,
+  containerRef,
   component = "section",
   background,
   minHeight,
   children,
-}) => {
-  return React.createElement(
+}) =>
+  React.createElement(
     component,
     {
       id: id,
+      ref: containerRef,
       className: styles.container,
       style: {
         background: background,
-        minHeight: minHeight
+        minHeight: minHeight,
       } as React.CSSProperties,
     },
     <div className={styles.wrapper}>{children}</div>
   );
-};
 
 export default Container;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,39 +1,75 @@
+import React, { ReactElement, useEffect, useRef } from "react";
 import type { NextPage } from "next";
 import Head from "next/head";
 import dynamic from "next/dynamic";
 import Container from "../components/Container";
+import { useIntersectionObserver } from "usehooks-ts";
 
 const Hero = dynamic(() => import("../page-components/HeroSection"));
 const Beauty = dynamic(() => import("../page-components/BeautySection"));
 const Contact = dynamic(() => import("../page-components/ContactSection"));
 const Footer = dynamic(() => import("../components/Footer"));
 
-const Home: NextPage = () => (
-  <>
-    <Head>
-      <title>Joel Tumambo</title>
-      <meta
-        name="description"
-        content="Joel Tumambo is a software engineer based in Philippines.
-          He specializes in beautiful front-end and design system"
-      />
-      <meta name="viewport" content="width=device-width" />
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+const sections: {
+  id: string;
+  component: ReactElement;
+  background: string;
+}[] = [
+  {
+    id: "hero",
+    component: <Hero />,
+    background: "var(--brown-50)",
+  },
+  {
+    id: "learn",
+    component: <Beauty />,
+    background: "var(--grey-50)",
+  },
+  {
+    id: "contact",
+    component: <Contact />,
+    background: "var(--indigo-50)",
+  },
+];
 
-    <main>
-      <Container minHeight="90vh" background="var(--brown-50)">
-        <Hero />
-      </Container>
-      <Container minHeight="90vh" id="learn" background="var(--grey-50)">
-        <Beauty />
-      </Container>
-      <Container minHeight="90vh" id="contact" background="var(--indigo-50)">
-        <Contact />
-      </Container>
-    </main>
-    <Footer />
-  </>
-);
+const Home: NextPage = () => {
+  const refs = sections.map(() => React.useRef<HTMLElement>(null));
+  const entries = refs.map((ref) =>
+    useIntersectionObserver(ref, {
+      threshold: 0.1,
+      freezeOnceVisible: true,
+    })
+  );
+
+  return (
+    <>
+      <Head>
+        <title>Joel Tumambo</title>
+        <meta
+          name="description"
+          content="Joel Tumambo is a software engineer based in Philippines.
+            He specializes in beautiful front-end and design system"
+        />
+        <meta name="viewport" content="width=device-width" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        {sections.map(({ id, background, component }, index) => (
+          <Container
+            containerRef={refs[index]}
+            id={id}
+            key={id}
+            minHeight="90vh"
+            background={background}
+          >
+            {!!entries[index]?.isIntersecting ? component : "loading"}
+          </Container>
+        ))}
+      </main>
+      <Footer />
+    </>
+  );
+};
 
 export default Home;
