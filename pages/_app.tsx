eb6c25410770d6b84aa3f51f452c9cd2acb5d766
Joel Tumambo <joeltumambs@gmail.com>
2022-04-01 11:54:12 +0800

    Improve init code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,18 +2,20 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { useEffect } from "react";
 
-const makeEven = (n: number) => n % 2 === 0 ? n : n + 1;
+const makeEven = (n: number) => (n % 2 === 0 ? n : n + 1);
+
+const setHeightMeta = () => () => {
+  const metaViewport = document.querySelector("meta[name=viewport]");
+  if (metaViewport) {
+    metaViewport.setAttribute(
+      "content",
+      `height=${makeEven(innerHeight)}, width=device-width, initial-scale=1.0`
+    );
+  }
+};
 
 function MyApp({ Component, pageProps }: AppProps) {
-  useEffect(() => {
-    const metaViewport = document.querySelector("meta[name=viewport]");
-    if (metaViewport) {
-      metaViewport.setAttribute(
-        "content",
-        `height=${makeEven(innerHeight)}, width=device-width, initial-scale=1.0`
-      );
-    }
-  }, []);
+  useEffect(setHeightMeta, []);
 
   return <Component {...pageProps} />;
 }
