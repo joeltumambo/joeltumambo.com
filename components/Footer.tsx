984b39ecb934c97f39ae08c44ea184d40aafcc50
Joel Tumambo <joeltumambs@gmail.com>
2022-04-08 19:56:49 +0800

    Fix text contrast in Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import Container from "./Container";
 
 const Footer = () => (
   <Container component="footer" minHeight="10vh" background="var(--grey-900)">
-    <Typography gutter={0} color="var(--grey-500)" size={{ xs: -2, sm: -1 }}>
+    <Typography gutter={0} color="var(--grey-300)" size={{ xs: -2, sm: -1 }}>
       © {new Date().getFullYear()} Joel Tumambo
     </Typography>
   </Container>
