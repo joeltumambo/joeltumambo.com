038cefcf00c636cc60032f6bfc4ae98c960b8da2
Joel Tumambo <joeltumambs@gmail.com>
2022-04-09 19:10:28 +0800

    Fix top position by using vh instead of px

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,6 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useEventListener } from "usehooks-ts";
+import evenify from "../utils/evenify";
 import Button from "./Button";
 import Container from "./Container";
 import Icon from "./Icon";
@@ -8,17 +9,19 @@ const Header = () => {
   const [opacity, setOpacity] = useState(0);
   const [lastScrollTop, setLastScrollTop] = useState(0);
   const [top, setTop] = useState(0);
+  const [text, setText] = useState("");
 
   const onScroll = () => {
     const element = document.documentElement;
     const scrollTop = element.scrollTop;
-    const height = Math.max(element.clientHeight * 0.1, 52);
-    const scrollDelta = lastScrollTop - scrollTop;
+    const evenRelativeHeight = evenify(Math.round(element.clientHeight * 0.1));
+    const height = Math.max(evenRelativeHeight, 52);
+    const scrollDelta = (lastScrollTop - scrollTop) * 0.1;
 
     setLastScrollTop(scrollTop);
 
     if (scrollDelta < 0) {
-      setTop(Math.max(top + scrollDelta, height * -1));
+      setTop(Math.max(top + scrollDelta, -10));
     } else {
       setTop(Math.min(top + scrollDelta, 0));
     }
@@ -37,7 +40,7 @@ const Header = () => {
         zIndex: 100,
         height: "10vh",
         position: "sticky",
-        top: `${top}px`,
+        top: `${top}vh`,
         boxShadow: `0 1px 0 0 rgba(0, 0, 0, ${opacity / 10})`,
         backdropFilter: "blur(4px)",
         WebkitBackdropFilter: "blur(4px)",
