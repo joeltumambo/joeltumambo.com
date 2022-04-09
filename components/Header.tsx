cacb7816f2e83f0d1446aea5861717227ff28edc
Joel Tumambo <joeltumambs@gmail.com>
2022-04-09 21:51:24 +0800

    Fix header on small screens

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useEventListener } from "usehooks-ts";
+import { useEventListener, useTimeout } from "usehooks-ts";
 import evenify from "../utils/evenify";
 import Button from "./Button";
 import Container from "./Container";
@@ -8,22 +8,30 @@ import Icon from "./Icon";
 const Header = () => {
   const [opacity, setOpacity] = useState(0);
   const [lastScrollTop, setLastScrollTop] = useState(0);
-  const [top, setTop] = useState(0);
+  const [top, setTop] = useState({
+    value: 0,
+    unit: "",
+  });
+  // let scrollTimer: number | undefined;
 
   const onScroll = () => {
     const element = document.documentElement;
     const scrollTop = element.scrollTop;
     const evenRelativeHeight = evenify(Math.round(element.clientHeight * 0.1));
     const height = Math.max(evenRelativeHeight, 52);
-    const scrollDelta = (lastScrollTop - scrollTop) * 0.1;
+    const maxTop = height === 52 ? -52 : -10;
+    const deltaFactor = height === 52 ? 1 : 0.1;
+    const scrollDelta = (lastScrollTop - scrollTop) * deltaFactor;
+    const newTop = top.value + scrollDelta;
+    const isUp = scrollDelta < 0;
+    const value = isUp ? Math.max(newTop, maxTop) : Math.min(newTop, 0);
+    const unit = height === 52 ? "px" : "vh";
 
     setLastScrollTop(scrollTop);
-
-    if (scrollDelta < 0) {
-      setTop(Math.max(top + scrollDelta, -10));
-    } else {
-      setTop(Math.min(top + scrollDelta, 0));
-    }
+    setTop({
+      value: value,
+      unit: unit,
+    });
     setOpacity(Math.min(1 - (height - scrollTop) / height, 1));
   };
 
@@ -39,7 +47,7 @@ const Header = () => {
         zIndex: 100,
         height: "10vh",
         position: "sticky",
-        top: `${top}vh`,
+        top: `${top.value}${top.unit}`,
         boxShadow: `0 1px 0 0 rgba(0, 0, 0, ${opacity / 10})`,
         backdropFilter: "blur(4px)",
         WebkitBackdropFilter: "blur(4px)",
