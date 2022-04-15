dc4c1a07d9a4ae3771d0efeb88c54508063b395f
Joel Tumambo <joeltumambs@gmail.com>
2022-04-16 04:00:56 +0800

    Fix hello link

--- components/Header.tsx
+++ components/Header.tsx
@@ -93,7 +93,7 @@ const Header = () => {
             size="small"
             filled
             link={{
-              href: "#contact",
+              href: "/#contact",
             }}
           >
             Say hello!
